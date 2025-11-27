22bab35c7ca6e824da965775971b309c3041e990
quentinmss18 <quentinmoussous@gmail.com>
2025-11-27 15:54:48 +0000

    main.h

--- main.h
+++ main.h
@@ -5,5 +5,9 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int _putchar(char c);
+int print_int(int n);
 
-#endif /* MAIN_H */
+#endif 
